d3c90bb2ea7d6c65a9feeb58208ee7a7dd13ff91
Carl Boettiger <cboettig@gmail.com>
2015-03-30 13:00:54 -0700

    Use fix in rmarkdown to allow '@' symbols in email,
    see issue https://github.com/rstudio/rmarkdown/issues/372#issuecomment-87413852

--- R/elsevier_article.R
+++ R/elsevier_article.R
@@ -1,6 +1,13 @@
+#' elsevier
+
+#' use the Elsevier template
+#'
+#' @param ... additional arguments to \code{rmarkdown::pdf_document}
 #' @export
-elsevier <- function() {
+elsevier <- function(...) {
   template <- find_resource("elsevier", "template.tex")
-  base <- rmarkdown::pdf_document(template = template, keep_tex = TRUE)
+
+  md_extensions <- c("+ascii_identifiers", "+tex_math_single_backslash", "-autolink_bare_uris")
+  base <- rmarkdown::pdf_document(template = template, keep_tex = TRUE, md_extensions = md_extensions, ...)
   base
 }
