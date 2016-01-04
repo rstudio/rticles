a4837367396032c3ca21e618e370b9bb983d8c1a
JJ Allaire <jj@rstudio.org>
2016-01-04 08:03:02 -0500

    rev acs_article

--- R/acs_article.R
+++ R/acs_article.R
@@ -2,16 +2,18 @@
 #'
 #' Format for creating an American Chemical Society (ACS) articles. Adapted from \href{http://pubs.acs.org/page/4authors/submission/tex.html}{http://pubs.acs.org/page/4authors/submission/tex.html}.
 #'
+#'
 #' @inheritParams rmarkdown::pdf_document
+#' @param ... Additional arguments to \code{rmarkdown::pdf_document}
+#'
+#' @return R Markdown output format to pass to \code{\link[rmarkdown:render]{render}}
 #'
 #' @export
-acs <- function(keep_tex = TRUE) {
-  template <- find_resource("acs", "template.tex")
-  csl <- find_resource("acs", "american-chemical-society.csl")
-
-  rmarkdown::pdf_document(
-    keep_tex = keep_tex,
-    fig_caption = TRUE,
-    template = template,
-    pandoc_args = c("--csl", rmarkdown::pandoc_path_arg(csl)))
+acs_article <- function(..., fig_caption = TRUE, keep_tex = TRUE) {
+  pdf_document_format(...,
+                      fig_caption = fig_caption,
+                      keep_tex = keep_tex,
+                      format = "acs_article",
+                      template = "template.tex",
+                      csl = "american-chemical-society.csl")
 }
