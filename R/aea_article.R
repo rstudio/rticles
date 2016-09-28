44a29785c8690066ec0932ecf7f6e988551bff3b
Sam <sboysel@gmail.com>
2016-09-28 12:59:45 -0700

    add templates for American Economic Association journals

---
+++ R/aea_article.R
@@ -0,0 +1,27 @@
+#' American Economic Association journal submissions.
+#'
+#' Format for creating submissions to the American Economic Association (AER, AEJ, JEL, PP). Adapted from
+#' \href{https://www.aeaweb.org/journals/policies/templates}{https://www.aeaweb.org/journals/policies/templates}.
+#'
+#' @inheritParams rmarkdown::pdf_document
+#' @param ... Additional arguments to \code{rmarkdown::pdf_document}
+#'
+#' @return R Markdown output format to pass to
+#'   \code{\link[rmarkdown:render]{render}}
+#'
+#' @examples
+#'
+#' \dontrun{
+#' library(rmarkdown)
+#' draft("MyArticle.Rmd", template = "aea_article", package = "rticles")
+#' }
+#'
+#' @export
+aea_article <- function(...,
+                        keep_tex = TRUE,
+                        md_extensions = c("-autolink_bare_uris")) {
+  inherit_pdf_document(...,
+                       template = find_resource("aea_article", "template.tex"),
+                       keep_tex = keep_tex,
+                       md_extensions = md_extensions)
+}
