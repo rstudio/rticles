8e12dd44bc872209f4b2d0d9ab8ef85cde4333eb
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2017-06-27 14:45:52 -0500

    update article with pandoc headers so it works

--- R/peerj_article.R
+++ R/peerj_article.R
@@ -4,6 +4,10 @@
 #'
 #' @inheritParams rmarkdown::pdf_document
 #' @param ... Additional arguments to \code{rmarkdown::pdf_document}
+#' @param base_format The function to use for the base format of the article. 
+#'   By default, this is \code{rmarkdown::pdf_document}, but to use pandoc's 
+#'   cross-referencing feature, this can be set to \code{bookdown::pdf_document2}
+#' 
 #'
 #' @return R Markdown output format to pass to
 #'   \code{\link[rmarkdown:render]{render}}
@@ -16,8 +20,13 @@
 #' }
 #'
 #' @export
-peerj_article <- function(..., keep_tex = TRUE) {
-  inherit_pdf_document(...,
-                       keep_tex = keep_tex,
-                       template = find_resource("peerj_article", "template.tex"))
+peerj_article <- function(..., keep_tex = TRUE, base_format = rmarkdown::pdf_document) {
+  if (inherits(base_format, "character")){
+  	FMT <- eval(parse(text = base_format))
+  } else {
+    FMT <- match.fun(base_format)
+  }
+  out <- FMT(...,
+             keep_tex = keep_tex,
+             template = find_resource("peerj_article", "template.tex"))
 }
