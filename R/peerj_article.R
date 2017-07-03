3219756ccf1ff0b97d04ee617281fd3c6352a5dd
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2017-07-03 10:07:17 -0500

    update skeleton and documentation for peerj
    The skeleton had an extra line and an incorrect ref

--- R/peerj_article.R
+++ R/peerj_article.R
@@ -3,15 +3,21 @@
 #' Format for creating submissions to The PeerJ.
 #'
 #' @inheritParams rmarkdown::pdf_document
-#' @param ... Additional arguments to \code{rmarkdown::pdf_document}
+#' @param ... Additional arguments to \code{base_format}
 #' @param base_format The function to use for the base format of the article.
-#'   By default, this is \code{rmarkdown::pdf_document}, but to use pandoc's
+#'   By default, this is \code{rmarkdown::pdf_document}, but to use bookdown's
 #'   cross-referencing feature, this can be set to \code{bookdown::pdf_document2}
-#'
 #'
 #' @return R Markdown output format to pass to
 #'   \code{\link[rmarkdown:render]{render}}
 #'
+#' @details This was adapted from the 
+#' \href{https://www.overleaf.com/latex/templates/latex-template-for-peerj-journal-and-pre-print-submissions/ptdwfrqxqzbn}{PeerJ Overleaf Template}.
+#' 
+#' @note If you use \code{rmarkdown::pdf_document()}, all internal references 
+#' (i.e. tables and figures) must use \code{\ref\{\}} whereas with 
+#' \code{bookdown::pdf_document2()}, you can additionally use \code{\@@ref()}.
+#' 
 #' @examples
 #'
 #' \dontrun{
