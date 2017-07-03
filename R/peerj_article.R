4b658f1db3329fbf674c3df687556e5daf542901
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2017-07-03 10:17:59 -0500

    fix formatting issue

--- R/peerj_article.R
+++ R/peerj_article.R
@@ -11,13 +11,13 @@
 #' @return R Markdown output format to pass to
 #'   \code{\link[rmarkdown:render]{render}}
 #'
-#' @details This was adapted from the 
+#' @details This was adapted from the
 #' \href{https://www.overleaf.com/latex/templates/latex-template-for-peerj-journal-and-pre-print-submissions/ptdwfrqxqzbn}{PeerJ Overleaf Template}.
-#' 
-#' @note If you use \code{rmarkdown::pdf_document()}, all internal references 
-#' (i.e. tables and figures) must use \code{\ref\{\}} whereas with 
-#' \code{bookdown::pdf_document2()}, you can additionally use \code{\@@ref()}.
-#' 
+#'
+#' @note If you use \code{rmarkdown::pdf_document()}, all internal references
+#' (i.e. tables and figures) must use \code{\\ref\{\}} whereas with
+#' \code{bookdown::pdf_document2()}, you can additionally use \code{\\@@ref()}.
+#'
 #' @examples
 #'
 #' \dontrun{
