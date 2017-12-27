6f1b6ce109fc33b052bb0ca5f713f0620a73ff49
dleutnant <leutnant@fh-muenster.de>
2017-12-27 22:28:38 +0100

    move csl out of YAML header

--- R/mdpi_article.R
+++ R/mdpi_article.R
@@ -17,7 +17,9 @@
 #'
 #' @export
 mdpi_article <- function(..., keep_tex = TRUE) {
-  inherit_pdf_document(...,
-                       keep_tex = keep_tex,
-                       template = find_resource("mdpi_article", "template.tex"))
+  pdf_document_format(...,
+                      keep_tex = keep_tex,
+                      format = "mdpi_article",
+                      template = "template.tex",
+                      csl = "multidisciplinary-digital-publishing-institute.csl")
 }
