5d89bb5ddce8827048544853a13c16a9de54d349
Mika Braginsky <mika.br@gmail.com>
2018-05-30 12:55:02 -0400

    add conll 2018 template

---
+++ R/conll_article.R
@@ -0,0 +1,21 @@
+#' CoNLL paper format
+#'
+#' @inheritParams rmarkdown::pdf_document
+#' @param ... Additional arguments to \code{rmarkdown::pdf_document}
+#'
+#' @return R Markdown output format to pass to
+#'   \code{\link[rmarkdown:render]{render}}
+#'
+#' @examples
+#'
+#' \dontrun{
+#' library(rmarkdown)
+#' draft("MyArticle.Rmd", template = "conll_article", package = "rticles")
+#' }
+#'
+#' @export
+conll_article <- function(..., keep_tex = TRUE) {
+  inherit_pdf_document(...,
+                       keep_tex = keep_tex,
+                       template = find_resource("conll_article", "template.tex"))
+}
