22d66eb9b1b71999c2c3f18f558bdb4a01eba2fc
Mika Braginsky <mika.br@gmail.com>
2018-08-14 15:21:36 -0400

    add tacl template

---
+++ R/tacl_article.R
@@ -0,0 +1,21 @@
+#' TACL paper format
+#'
+#' @inheritParams rmarkdown::pdf_document
+#' @param ... Additional arguments to \code{rmarkdown::pdf_document}
+#'
+#' @return R Markdown output format to pass to
+#'   \code{\link[rmarkdown:render]{render}}
+#'
+#' @examples
+#'
+#' \dontrun{
+#' library(rmarkdown)
+#' draft("MyArticle.Rmd", template = "conll_article", package = "rticles")
+#' }
+#'
+#' @export
+tacl_article <- function(..., keep_tex = TRUE) {
+  inherit_pdf_document(...,
+                       keep_tex = keep_tex,
+                       template = find_resource("tacl_article", "template.tex"))
+}
