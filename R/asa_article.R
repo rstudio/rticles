16302b060895475c8709bf9e2fc3c445678e7f5d
earowang <earo.wang@gmail.com>
2019-03-12 14:02:55 +1100

    asa_article() gains a new argument 'base_format' to support bookdown::pdf_document2()

--- R/asa_article.R
+++ R/asa_article.R
@@ -5,17 +5,26 @@
 #'
 #' @inheritParams rmarkdown::pdf_document
 #' @param ... Arguments to \code{rmarkdown::pdf_document}
+#' @inheritParams peerj_article
 #' @return R Markdown output format to pass to \code{\link[rmarkdown:render]{render}}
+#' @note If you use \code{rmarkdown::pdf_document()}, all internal references
+#' (i.e. tables and figures) must use \code{\\ref\{\}} whereas with
+#' \code{bookdown::pdf_document2()}, you can additionally use \code{\\@@ref()}.
 #' @examples
 #' \dontrun{
 #' rmarkdown::draft("MyArticle.Rmd", template = "asa_article", package = "rticles")
 #' }
 #'
 #' @export
-asa_article <- function(..., keep_tex = TRUE, citation_package = 'natbib'){
-  template <- find_resource("asa_article", "template.tex")
-  fmt <- inherit_pdf_document(template = template,
-                              keep_tex = keep_tex,
-                              citation_package = citation_package,
-                              ...)
+asa_article <- function(..., keep_tex = TRUE, citation_package = 'natbib',
+                        base_format = rmarkdown::pdf_document) {
+  if (inherits(base_format, "character")){
+    FMT <- eval(parse(text = base_format))
+  } else {
+    FMT <- match.fun(base_format)
+  }
+  out <- FMT(...,
+             citation_package = citation_package,
+             keep_tex = keep_tex,
+             template = find_resource("asa_article", "template.tex"))
 }
