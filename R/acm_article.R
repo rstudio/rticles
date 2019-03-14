6bb22143ae2f9fcdf13ad03fc6792f009433e83d
Yihui Xie <xie@yihui.name>
2019-03-14 17:20:58 -0500

    cosmetic changes in acm_article()

--- R/acm_article.R
+++ R/acm_article.R
@@ -2,25 +2,20 @@
 #'
 #' Format for creating an Association for Computing Machinery (ACM) articles.
 #' Adapted from
-#' \href{http://www.acm.org/publications/article-templates/proceedings-template.html}{http://www.acm.org/publications/article-templates/proceedings-template.html}.
+#' \url{http://www.acm.org/publications/article-templates/proceedings-template.html}.
 #'
 #' @inheritParams rmarkdown::pdf_document
-#' @param ... Arguments to \code{rmarkdown::pdf_document}
-#'
-#' @return R Markdown output format to pass to \code{\link[rmarkdown:render]{render}}
-#'
+#' @param ... Arguments to \code{rmarkdown::pdf_document()}.
+#' @return An R Markdown output format.
 #' @examples
-#'
 #' \dontrun{
-#' library(rmarkdown)
-#' draft("MyArticle.Rmd", template = "acm_article", package = "rticles")
+#' rmarkdown::draft("MyArticle.Rmd", template = "acm_article", package = "rticles")
 #' }
-#'
 #' @export
 acm_article <- function(...) {
-  pdf_document_format(...,
-                      format = "acm_article",
-                      template = "template.tex",
-                      csl = "acm-sig-proceedings.csl")
+  pdf_document_format(
+    ..., format = "acm_article", template = "template.tex",
+    csl = "acm-sig-proceedings.csl"
+  )
 }
 
