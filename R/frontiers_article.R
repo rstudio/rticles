8be2edc73dd45e230fcb1d7757d638042bbeeec8
muschellij2 <muschellij2@gmail.com>
2019-03-15 15:50:45 -0400

    added in the ability for different documentclasses

--- R/frontiers_article.R
+++ R/frontiers_article.R
@@ -13,7 +13,16 @@
 frontiers_article <- function(...,
                               keep_tex = TRUE) {
 
-  inherit_pdf_document(...,
+  x = inherit_pdf_document(...,
                        template = find_resource("frontiers_article", "template.tex"),
                        keep_tex = keep_tex)
+  documentclass = x$documentclass
+  documentclass[documentclass %in% "" ] = NULL
+  if (is.null(documentclass)) {
+    documentclass = "frontiersSCNS"
+  }
+  x$pandoc$args = c(x$pandoc$args,
+                    c("--variable", paste0("documentclass=", documentclass))
+  )
+  x
 }
