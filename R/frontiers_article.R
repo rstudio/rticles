1ccafab55e40533230bd67e67b5c4aa8a3ecde1e
muschellij2 <muschellij2@gmail.com>
2019-03-15 16:20:43 -0400

    allowed for documentclass to be fixed

--- R/frontiers_article.R
+++ R/frontiers_article.R
@@ -5,22 +5,27 @@
 #'
 #' @inheritParams rmarkdown::pdf_document
 #' @param ... Additional arguments to \code{rmarkdown::pdf_document}
-#'
+#' @param documentclass The document class for the Frontiers In Article:
+#' \describe{
+#' \item{\code{frontiersSCNS}}{Science, Engineering and Humanities and
+#' Social Sciences articles}
+#' \item{\code{frontiersHLTH}}{Health articles}
+#' \item{\code{frontiersFPHY}}{Physics and Applied Mathematics
+#' and Statistics articles}
+#' }
 #' @return R Markdown output format to pass to
 #'   \code{\link[rmarkdown:render]{render}}
 #'
 #' @export
-frontiers_article <- function(...,
-                              keep_tex = TRUE) {
+frontiers_article <- function(
+  ...,
+  keep_tex = TRUE,
+  documentclass = c("frontiersSCNS", "frontiersHLTH", "frontiersFPHY")) {
 
   x = inherit_pdf_document(...,
-                       template = find_resource("frontiers_article", "template.tex"),
-                       keep_tex = keep_tex)
-  documentclass = x$documentclass
-  documentclass[documentclass %in% "" ] = NULL
-  if (is.null(documentclass)) {
-    documentclass = "frontiersSCNS"
-  }
+                           template = find_resource("frontiers_article", "template.tex"),
+                           keep_tex = keep_tex)
+  documentclass = match.arg(documentclass)
   x$pandoc$args = c(x$pandoc$args,
                     c("--variable", paste0("documentclass=", documentclass))
   )
