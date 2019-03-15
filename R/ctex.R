42d2563d4cc994ea712385c99c7e862ced0b026c
Yihui Xie <xie@yihui.name>
2019-03-15 15:28:52 -0500

    rename the template default.latex to template.tex for ctex(), and remove the unused ctex_template()

--- R/ctex.R
+++ R/ctex.R
@@ -3,26 +3,9 @@
 #' \code{ctex()} is a wrapper function for \code{rmarkdown::pdf_document()} and
 #' changed the default values of two arguments \code{template} and
 #' \code{latex_engine} so it works better with the \pkg{ctex} package.
-#' @param ...,template,latex_engine Passed to
-#'   \code{markdown::\link[rmarkdown]{pdf_document}()}
-#' @return \code{ctex()} returns a format that can be passed to
-#'   \code{rmarkdown::render()}; \code{ctex_template()} returns the path to a
-#'   LaTeX template in \pkg{rticles} for Chinese documents using the \pkg{ctex}
-#'   package.
-#'
-#' @examples
-#'
-#' \dontrun{
-#' library(rmarkdown)
-#' draft("MyArticle.Rmd", template = "ctex", package = "rticles")
-#' }
-#'
+#' @param ...,latex_engine Passed to
+#'   \code{markdown::\link[rmarkdown]{pdf_document}()}.
 #' @export
-ctex <- function(..., template = ctex_template(), latex_engine = 'xelatex') {
-  inherit_pdf_document(..., template = template, latex_engine = latex_engine)
+ctex <- function(..., latex_engine = 'xelatex') {
+  pdf_document_format('ctex', latex_engine = latex_engine, ...)
 }
-
-
-#' @rdname ctex
-#' @export
-ctex_template <- function() find_resource('ctex', 'default.latex')
