bde4dda3e4bcb9e0a6dac99184ffdc26490e5e30
Yihui Xie <xie@yihui.name>
2019-03-14 22:44:30 -0500

    merge find_file() into find_resource()

--- R/utils.R
+++ R/utils.R
@@ -1,15 +1,11 @@
-find_file <- function(template, file) {
-  template <- system.file("rmarkdown", "templates", template, file,
-                          package = "rticles")
-  if (template == "") {
-    stop("Couldn't find template file ", template, "/", file, call. = FALSE)
-  }
-
-  template
-}
-
 find_resource <- function(template, file) {
-  find_file(template, file.path("resources", file))
+  res <- system.file(
+    "rmarkdown", "templates", template, "resources", file, package = "rticles"
+  )
+  if (res == "") stop(
+    "Couldn't find template file ", template, "/resources/", file, call. = FALSE
+  )
+  res
 }
 
 knitr_fun <- function(name) utils::getFromNamespace(name, 'knitr')
