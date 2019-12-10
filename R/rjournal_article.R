rstudio/rticles

commit 56028df8d5fb2b580f6534c0ce155211a37f3514

pandoc 2.x will escape % in the YAML field, so we have to use the `includes` option

inst/rmarkdown/templates/rjournal_article/skeleton/preamble.tex
@@ -1,0 +1,1 @@
+% Any extra LaTeX you need in the preamble

inst/rmarkdown/templates/rjournal_article/skeleton/skeleton.Rmd
@@ -15,9 +15,10 @@ author:
     email:  author2@work
 abstract: >
   An abstract of less than 150 words.
-preamble: >
-  % Any extra latex you need in the preamble
-output: rticles::rjournal_article
+output:
+  rticles::rjournal_article:
+    includes:
+      in_header: preamble.tex
 ---
 
 ## Introduction

inst/rmarkdown/templates/rjournal_article/resources/RJwrapper.tex
@@ -8,8 +8,9 @@
 \providecommand{\tightlist}{%
   \setlength{\itemsep}{0pt}\setlength{\parskip}{0pt}}
 
-%% load any required packages here
-$preamble$
+$for(header-includes)$
+$header-includes$
+$endfor$
 
 \begin{document}
 

commit ebc84d8c365021b24c6446e0a37af85274f98c71

Fix #257: header includes for rjournal_article() was broken in 56028df8d5fb2b580f6534c0ce155211a37f3514 (#261)
Now we also support three ways to include LaTeX code in the preamble: using the `preamble` field in YAML, or the `header-includes` field, or the `includes` option of `rticles::rjournal_article`

DESCRIPTION
@@ -35,7 +35,8 @@ Authors@R: c(
   person("Elio", "Campitelli", role = c("aut", "cph"), email = "elio.campitelli@cima.fcen.uba.ar", comment = c(ORCID = "0000-0002-7742-9230")),
   person("John", "Muschelli", role = c("aut", "cph"), email = "muschellij2@gmail.com", comment = c(ORCID = "0000-0001-6469-1750")),
   person("Zhian N.", "Kamvar", role = c("aut", "cph"), email = "zkamvar@gmail.com", comment = c(ORCID = "0000-0003-1458-7108")),
-  person("Noam", "Ross", role = c("aut", "cph"), email = "noam.ross@gmail.com", comment = c(ORCID = "0000-0002-2136-0000"))
+  person("Noam", "Ross", role = c("aut", "cph"), email = "noam.ross@gmail.com", comment = c(ORCID = "0000-0002-2136-0000")),
+  person("Robrecht", "Cannoodt", role = c("aut", "cph"), email = "rcannood@gmail.com", comment = c(ORCID = "0000-0003-3641-729X", github = "rcannood"))
   )
 Description: A suite of custom R Markdown formats and templates for
   authoring journal articles and conference submissions.

R/rjournal_article.R
@@ -2,14 +2,15 @@
 #'
 #' Format for creating R Journal articles. Adapted from
 #' \url{https://journal.r-project.org/submissions.html}.
-#' @inheritParams rmarkdown::pdf_document
-#' @param ... Arguments to \code{rmarkdown::pdf_document}
+#' @param ...,citation_package Arguments to \code{rmarkdown::pdf_document}.
 #' @export
-rjournal_article <- function(...) {
+rjournal_article <- function(..., citation_package = 'natbib') {
 
   rmarkdown::pandoc_available('2.2', TRUE)
 
-  base <- pdf_document_format("rjournal_article", highlight = NULL, ...)
+  base <- pdf_document_format(
+    "rjournal_article", highlight = NULL, citation_package = citation_package, ...
+  )
 
   # Render will generate tex file, post-process hook generates appropriate
   # RJwrapper.tex and use pandoc to build pdf from that
@@ -24,10 +25,15 @@ rjournal_article <- function(...) {
     if (filename != (filename2 <- gsub('_', '-', filename))) {
       file.rename(filename, filename2); filename <- filename2
     }
-    wrapper_metadata <- list(preamble = metadata$preamble, filename = xfun::sans_ext(filename))
-    wrapper_template <- find_resource("rjournal_article", "RJwrapper.tex")
-    wrapper_output <- file.path(getwd(), "RJwrapper.tex")
-    template_pandoc(wrapper_metadata, wrapper_template, wrapper_output, verbose)
+    m <- list(filename = xfun::sans_ext(filename))
+    h <- get_list_element(metadata, c('output', 'rticles::rjournal_article', 'includes', 'in_header'))
+    h <- c(h, if (length(preamble <- unlist(metadata[c('preamble', 'header-includes')]))) {
+      f <- tempfile(fileext = '.tex'); on.exit(unlink(f), add = TRUE)
+      xfun::write_utf8(preamble, f)
+      f
+    })
+    t <- find_resource("rjournal_article", "RJwrapper.tex")
+    template_pandoc(m, t, "RJwrapper.tex", h, verbose)
 
     tinytex::latexmk("RJwrapper.tex", base$pandoc$latex_engine, clean = clean)
   }

R/utils.R
@@ -24,6 +24,7 @@ merge_list <- function(x, y) {
 #' @param metadata A named list containing metadata to pass to template.
 #' @param template Path to a pandoc template.
 #' @param output Path to save output.
+#' @param in_header Paths to files to include in the header.
 #' @return (Invisibly) The path of the generate file.
 #' @examples
 #' x <- rticles:::template_pandoc(
@@ -33,17 +34,19 @@ merge_list <- function(x, y) {
 #' )
 #' if (interactive()) file.show(x)
 #' @noRd
-template_pandoc <- function(metadata, template, output, verbose = FALSE) {
+template_pandoc <- function(metadata, template, output, in_header = NULL, verbose = FALSE) {
   tmp <- tempfile(fileext = ".md"); on.exit(unlink(tmp), add = TRUE)
   xfun::write_utf8(c("---", yaml::as.yaml(metadata), "---\n"), tmp)
 
   rmarkdown::pandoc_convert(
-    tmp, "markdown", output = output, verbose = verbose,
-    options = paste0("--template=", template),
+    tmp, output = output, verbose = verbose, wd = '.',
+    options = c(
+      "--template", rmarkdown::pandoc_path_arg(template),
+      rmarkdown::pandoc_include_args(in_header)
+    )
   )
   invisible(output)
 }
-
 
 # Helper function to create a custom format derived from pdf_document that
 # includes a custom LaTeX template
@@ -55,6 +58,10 @@ pdf_document_format <- function(
   fmt
 }
 
-
-
-
+# recursion into a list to get an element using a vector of names
+get_list_element <- function(x, names) {
+  n <- length(names)
+  if (!is.list(x) || n == 0) return()
+  for (i in names[seq_len(n - 1)]) if (!is.list(x <- x[[i]])) return()
+  x[[names[n]]]
+}

inst/rmarkdown/templates/rjournal_article/resources/template.tex
@@ -10,19 +10,6 @@ $abstract$
 }
 $endif$
 
-$if(csl-refs)$
-\newlength{\cslhangindent}
-\setlength{\cslhangindent}{1.5em}
-\newenvironment{cslreferences}%
-  {$if(csl-hanging-indent)$\setlength{\parindent}{0pt}%
-  \everypar{\setlength{\hangindent}{\cslhangindent}}\ignorespaces$endif$}%
-  {\par}
-$endif$
-
-$for(header-includes)$
-$header-includes$
-$endfor$
-
 $body$
 
 $if(bibliography)$

inst/rmarkdown/templates/rjournal_article/skeleton/RJournal.sty
@@ -201,7 +201,7 @@
   \noindent
   \begin{center}
     \fontsize{50pt}{50pt}\selectfont
-    The \raisebox{-8pt}{\includegraphics[height=77pt]{Rlogo-4}}\hspace{10pt}
+    The \raisebox{-8pt}{\includegraphics[height=77pt]{Rlogo}}\hspace{10pt}
     Journal
   
   \end{center}

inst/rmarkdown/templates/rjournal_article/skeleton/preamble.tex
@@ -1,1 +1,0 @@
-% Any extra LaTeX you need in the preamble

inst/rmarkdown/templates/rjournal_article/skeleton/skeleton.Rmd
@@ -15,27 +15,23 @@ author:
     email:  author2@work
 abstract: >
   An abstract of less than 150 words.
-output:
-  rticles::rjournal_article:
-    includes:
-      in_header: preamble.tex
+preamble: |
+  % Any extra LaTeX you need in the preamble
+output: rticles::rjournal_article
 ---
 
 ## Introduction
 
 Introductory section which may include references in parentheses
-\citep{R}, or cite a reference such as \citet{R} in the text.
+[@R], or cite a reference such as @R in the text.
 
 ## Section title in sentence case
 
-This section may contain a figure such as Figure \ref{figure:rlogo}.
+This section may contain a figure such as Figure \ref{fig:Rlogo}.
 
-\begin{figure}[htbp]
-  \centering
-  \includegraphics{Rlogo}
-  \caption{The logo of R.}
-  \label{figure:rlogo}
-\end{figure}
+```{r, Rlogo, echo=FALSE, fig.cap='The logo of R.', out.width='2in', fig.align='center', fig.pos='htbp'}
+knitr::include_graphics('Rlogo.pdf')
+```
 
 ## Another section
 
@@ -49,4 +45,5 @@ x
 ## Summary
 
 This file is only a basic article template. For full details of _The R Journal_ style and information on how to prepare your article for submission, see the [Instructions for Authors](https://journal.r-project.org/share/author-guide.pdf).
+
 \bibliography{RJreferences}

man/rjournal_article.Rd
@@ -4,10 +4,10 @@
 \alias{rjournal_article}
 \title{R Journal format.}
 \usage{
-rjournal_article(...)
+rjournal_article(..., citation_package = "natbib")
 }
 \arguments{
-\item{...}{Arguments to \code{rmarkdown::pdf_document}}
+\item{..., citation_package}{Arguments to \code{rmarkdown::pdf_document}.}
 }
 \description{
 Format for creating R Journal articles. Adapted from