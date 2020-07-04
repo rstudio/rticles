761cff08c9f947089639dd6632b78cdf4219194d
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2020-07-04 15:57:35 +0800

    Create bioinformatics journal
    Reference cannot work

--- R/article.R
+++ R/article.R
@@ -287,3 +287,14 @@ tf_article <- function(..., keep_tex = TRUE, citation_package = 'natbib') {
     "tf_article", keep_tex = keep_tex, citation_package = citation_package, ...
   )
 }
+
+#' @section \code{bioinformatics_article}: Format for creating submissions to a Bioinformatics journal. Adapted from
+#' \url{https://academic.oup.com/bioinformatics/pages/submission_online}.
+#' @export
+#' @rdname article
+bioinformatics_article <- function(..., keep_tex = TRUE, citation_package = 'natbib') {
+  pdf_document_format(
+    "bioinformatics_article", keep_tex = keep_tex, citation_package = citation_package,
+    md_extensions = "-auto_identifiers",...
+  )
+}
