51d27bf4c22e58d5f6939ddb5c28be7b1c34a5ae
Christophe Dervieux <christophe.dervieux@gmail.com>
2020-08-17 12:12:40 +0200

    use indices better to handles some edge cases with author lines and tests them

--- tests/testit/test-utils.R
+++ tests/testit/test-utils.R
@@ -1,8 +1,29 @@
 assert("last author is prepend with and ", {
+  # if no author line, unmodified
+  unmodified <- c("No author line", "returns unmodified")
+  (post_process_authors(unmodified) %==% unmodified)
+
+  # one author is unchanged
+  (post_process_authors("\\author{John Doe}") %==%
+      "\\author{John Doe}")
+
+  # when 2 or more, add a 'and'
   (post_process_authors(c("\\title{title}", "\\author{John, Bob}")) %==%
      c("\\title{title}", "\\author{John and Bob}"))
   (post_process_authors("\\author{John, Bob, Mary}") %==%
       "\\author{John, Bob, and Mary}")
-  unmodified <- c("No author line", "returns unmodified")
-  (post_process_authors(unmodified) %==% unmodified)
+
+  # Works also if authors on 2 or more lines in the tex file
+  (post_process_authors(
+    c("\\author{John, Bob,", "Mary, Dany}", "\\abstract{text}")
+  ) %==%
+      c("\\author{John, Bob,", "Mary, and Dany}", "\\abstract{text}"))
+  (post_process_authors(
+    c("\\title{title}", "\\author{John, Bob,", "Lucy, Ann,", "Mary, Dany}")
+  ) %==%
+      c("\\title{title}", "\\author{John, Bob,", "Lucy, Ann,", "Mary, and Dany}"))
+
+  # handles the weird case where two or more \\authors are used in the document
+  unmodified <- c("\\author{John, Bob}", "some text", "\\author{Mary, Dany}")
+  (suppressWarnings(post_process_authors(unmodified)) %==% unmodified)
 })
