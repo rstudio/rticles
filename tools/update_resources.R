07c509412d012cb2d9bad3f1b2ba671efca3a925
christophe dervieux <christophe.dervieux@gmail.com>
2020-12-15 17:39:38 +0100

    Also update other resources

--- tools/update_resources.R
+++ tools/update_resources.R
@@ -7,13 +7,12 @@ bundle <- "https://www.jstatsoft.org/public/journals/1/jss-article-tex.zip"
 new_jss <- xfun::in_dir(tmp_dir, {
   xfun::download_file(bundle)
   unzip(basename(bundle))
-  xfun::normalize_path("jss.cls")
+  xfun::normalize_path(
+    c(cls = "jss.cls", bst = "jss.bst", logo = "jsslogo.jpg")
+  )
 })
 file.copy(new_jss,
-          "inst/rmarkdown/templates/jss/skeleton/jss.cls",
+          "inst/rmarkdown/templates/jss/skeleton/",
           overwrite = TRUE)
 unlink(tmp_dir, recursive = TRUE)
 # Check git diff and commit
-
-
-
