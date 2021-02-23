0c04d2c23dd0e7f945cbbefcfbcca3e25320db85
christophe dervieux <christophe.dervieux@gmail.com>
2021-02-23 11:48:24 +0100

    be sure to pass a charcter vector of args

--- R/article.R
+++ R/article.R
@@ -207,7 +207,9 @@ ims_article <- function(journal = c("aoas", "aap", "aop", "aos", "sts"),
     )
 
   # Convert to pandoc arguments
-  pandoc_arg_list <- mapply(rmarkdown::pandoc_variable_arg, names(args), args)
+  pandoc_arg_list <- mapply(rmarkdown::pandoc_variable_arg, names(args), args,
+                            SIMPLIFY = FALSE, USE.NAMES = FALSE)
+  pandoc_arg_list <- unlist(pandoc_arg_list)
 
   pdf_document_format(
     "ims", keep_tex = keep_tex, citation_package = citation_package,
