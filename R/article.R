0df31067305c23986fde1843ef8853cccf1f8640
Elio Campitelli <eliocampitelli@gmail.com>
2023-05-02 16:04:41 -0300

    Makes natbib mandatory

--- R/article.R
+++ R/article.R
@@ -489,11 +489,15 @@ sim_article <- function(..., highlight = NULL, citation_package = "natbib") {
 #'   Macro package for Springer Journals.
 #' @export
 #' @rdname article
-springer_article <- function(..., keep_tex = TRUE,
-                             citation_package = "natbib") {
+springer_article <- function(..., keep_tex = TRUE) {
+
+  # if (!rmarkdown::pandoc_available("2.10")) {
+  #   stop("`elsevier_article()` now requires a minimum of pandoc 2.10.")
+  # }
+
   pdf_document_format(
     "springer",
-    keep_tex = keep_tex, citation_package = citation_package, ...
+    keep_tex = keep_tex, citation_package = "natbib", ...
   )
 }
 
