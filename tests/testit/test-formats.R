c822214964ab4c7c3978c79c257de568187bed71
Christophe Dervieux <christophe.dervieux@gmail.com>
2024-11-27 15:03:20 +0100

    patch springer class to remove orcidlogo definition
    The logo file is not provided, so we use orcidlink package instead. And the command conflicts

--- tests/testit/test-formats.R
+++ tests/testit/test-formats.R
@@ -78,8 +78,7 @@ test_format("rsos")
 test_format("rss")
 test_format("sage")
 test_format("sim")
-# FIXME: Fix issue with this format
-# test_format("springer", skip = !rmarkdown::pandoc_available("2.11.4"))
+test_format("springer", skip = !rmarkdown::pandoc_available("2.11.4"))
 test_format("tf")
 test_format("trb")
 # Deactivate because of https://github.com/rstudio/rticles/issues/523
